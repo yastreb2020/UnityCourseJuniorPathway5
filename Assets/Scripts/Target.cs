9096a015170f51266769732c625fb9566ba97b21
Katya Yastrebkova <katya2014ya@gmail.com>
2023-04-23 18:57:55 +0300

    prefabs created

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+
+    private float xRange = 4.5f;
+    private float ySpawnPos = -5;
+    private float minForce = 12;
+    private float maxForce = 16;
+    private float torqueRange = 10;
+
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        transform.position = RandomPosition();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private Vector3 RandomTorque()
+    {
+        return new Vector3(Random.Range(-torqueRange, torqueRange), Random.Range(-torqueRange, torqueRange), Random.Range(-torqueRange, torqueRange));
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
+    }
+
+    private Vector3 RandomPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
